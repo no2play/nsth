e9d96307ebf2e2272f8cfeabeba94a91a7e1753b
no2play <69761867+no2play@users.noreply.github.com>
2024-08-20 16:10:19 +0700

    Create Create_Users_domain.ps1

---
+++ Stellar-CPSDR/Create_Users_domain.ps1
@@ -0,0 +1,12 @@
+# Define the new domain user details
+$Username = "Attacker"
+$Password = ConvertTo-SecureString "P@ssw0rd123" -AsPlainText -Force
+$OU = "OU=Users,DC=sentinel,DC=local"  # Replace with the appropriate OU for your domain
+
+# Create a new domain user
+New-ADUser -Name $Username -AccountPassword $Password -Path $OU -Enabled $true -PasswordNeverExpires $true -UserPrincipalName "$Username@example.com" -SamAccountName $Username
+
+# Add the new user to the Domain Admins group
+Add-ADGroupMember -Identity "Domain Admins" -Members $Username
+
+Write-Host "User $Username created and added to Domain Admins group successfully."
